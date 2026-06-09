3aca62c7a7b2c16c8cc142b2e1cf03bc751f70da
Lucas <lmm7@cesar.school>
2026-06-09 09:04:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botaoTema = document.getElementById('botao-tema');
+    const body = document.body;
+
+    if (localStorage.getItem('tema') === 'claro') {
+        body.classList.add('modo-claro');
+        if (botaoTema) botaoTema.textContent = ' Modo Escuro';
+    }
+
+    if (botaoTema) {
+        botaoTema.addEventListener('click', () => {
+            body.classList.toggle('modo-claro');
+            
+            if (body.classList.contains('modo-claro')) {
+                botaoTema.textContent = ' Modo Escuro';
+                localStorage.setItem('tema', 'claro');
+            } else {
+                botaoTema.textContent = ' Modo Claro';
+                localStorage.setItem('tema', 'escuro');
+            }
+        });
+    }
+});
